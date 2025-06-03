2290929dc13d582b9d32d418095dd7fb553f4f1a
Abtahi <nawarabtahi@gmail.com>
2025-06-03 22:20:25 +0600

    Updates main.c file

--- main.c
+++ main.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//user management system
+void registeruser();
+void logoutuser();
+
 
 int main(){
-  printf("Hello world!");
+  
+  int choice;
+
+  while (1) {
+    system("cls");
+    printf("~-User Management System\n\n");
+    printf("1. Register\n");
+    printf("2. Logout\n");
+    printf("3. exit\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+      case 1:
+        registeruser();
+        break;
+      case 2:
+        logoutuser();
+        break;
+      case 3:
+        exit(0);
+      default:
+        printf("Wrong input! Press any key to try again...");
+        while ((getchar()) != '\n');
+        getchar();
+        break;
+    }
+ 
+  }
+
   return 0;
+}
+
+void registeruser(){
+
+}
+
+void logoutuser(){
+
 }
