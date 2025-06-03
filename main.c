1b8699593946bb548a7dd3fecc8bd846bcac24cc
Abtahi <nawarabtahi@gmail.com>
2025-06-03 23:53:35 +0600

    updated file before leaving

--- main.c
+++ main.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void registeruser();
-void logoutuser();
+#define USERLENGTH 10
+#define PASSWORDLENGTH 16
 
+typedef struct{
+  char username[USERLENGTH];
+  char password[PASSWORDLENGTH];
+} User;
+
+User users;
+
+FILE *userFile;
+
+void registerUser();
+void loginUser();
 
 int main(){
   
-  int choice;
-
   while (1) {
+    int choice;
     system("cls");
-    printf("~-User Management System\n\n");
+    printf("~-User Management System-~\n\n");
     printf("1. Register\n");
-    printf("2. Logout\n");
+    printf("2. Logoin\n");
     printf("3. exit\n");
 
     printf("Enter your choice: ");
@@ -21,10 +32,10 @@ int main(){
 
     switch (choice) {
       case 1:
-        registeruser();
+        registerUser();
         break;
       case 2:
-        logoutuser();
+        loginUser();
         break;
       case 3:
         exit(0);
@@ -40,10 +51,23 @@ int main(){
   return 0;
 }
 
-void registeruser(){
+void registerUser(){
+  system("cls");
+  printf("~-Register-~\n\n");
 
+  userFile = fopen("user.txt", "a");
+
+  while ((getchar()) != '\n');
+  printf("Enter your name: ");
+  fgets(users.username, USERLENGTH, stdin);
+  users.username[strcspn(users.username, "\n")] = 0;
+
+
+  fprintf(userFile, "%s\n", users.username);
+
+  fclose(userFile);
 }
 
-void logoutuser(){
+void loginUser(){
 
 }
